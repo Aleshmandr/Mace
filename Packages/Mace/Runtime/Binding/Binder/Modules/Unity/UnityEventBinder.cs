8e6739756489b9d006674a4f92ce21c9646a70df
Aleshmandr <aleshmandr@gmail.com>
2024-09-11 23:02:54 +0200

    Unity event binder

---
+++ Packages/Mace/Runtime/Binding/Binder/Modules/Unity/UnityEventBinder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Mace
+{
+    public class UnityEventBinder : ComponentBinder
+    {
+        [SerializeField] private BindingInfo eventRaise = BindingInfo.Event();
+        [SerializeField] private UnityEvent onEventRaise;
+        [SerializeField] private BindingInfo variable = BindingInfo.Variable<object>();
+        [SerializeField] private UnityEvent onVariableChange;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            RegisterEvent(eventRaise).OnRaised(HandlePlayEventRaise);
+            RegisterVariable<object>(variable).OnChanged(HandleVariableChange);
+        }
+
+        private void HandleVariableChange(object value)
+        {
+            onVariableChange?.Invoke();
+        }
+
+        private void HandlePlayEventRaise()
+        {
+            onEventRaise?.Invoke();
+        }
+    }
+}
