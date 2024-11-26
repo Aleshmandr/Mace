71a87e0982dfda72a7016faac833e6b6f4292296
Aleshmandr <aleshmandr@gmail.com>
2024-11-26 18:06:45 +0100

    Command binder

---
+++ Packages/Mace/Runtime/Binding/Binder/Modules/Unity/CommandBinder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Mace
+{
+    public class CommandBinder : ComponentBinder
+    {
+        [SerializeField] private BindingInfo command = BindingInfo.Command();
+        private UnityEvent commandExecuteTrigger;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            RegisterCommand(command).AddExecuteTrigger(commandExecuteTrigger);
+        }
+
+        public void ExecuteCommand()
+        {
+            commandExecuteTrigger?.Invoke();
+        }
+    }
+}
