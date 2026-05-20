e85eea2c7e0882d6856f3dfbebd047048ebf53ab
Alexander Papchikhin <alexander@Alexanders-MacBook-Pro.local>
2026-05-20 17:36:55 +0200

    Scroll rect focus behavior improvements

--- Packages/Mace/Runtime/Binding/Binder/Modules/Unity/UGUI/ScrollRectBinder.cs
+++ Packages/Mace/Runtime/Binding/Binder/Modules/Unity/UGUI/ScrollRectBinder.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Mace.Utils;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace Mace
 {
@@ -11,8 +13,15 @@ namespace Mace
     {
         [SerializeField] private BindingInfo focusItem = BindingInfo.Variable<object>();
         [SerializeField] private Vector2 focusMargin;
-        [SerializeField] private bool resetScrollWhenNothingFocused = true;
+        [SerializeField] private Vector2 focusPointOffset;
+        [SerializeField] private NothingSelectedBehaviorMode nothingSelectedBehaviorMode;
         private ScrollRect scrollRect;
+
+        private enum NothingSelectedBehaviorMode : byte
+        {
+            ResetScroll = 0,
+            KeepCurrent = 1,
+        }
 
         protected override void Awake()
         {
@@ -33,9 +42,9 @@ namespace Mace
         {
             if (itemViewModel == null)
             {
-               return false;
+                return false;
             }
-            
+
             RectTransform parentTransform = scrollRect.content;
             int childCount = parentTransform.childCount;
             for (int i = 0; i < childCount; i++)
@@ -44,25 +53,70 @@ namespace Mace
                 ViewModelComponent viewModelComponent = child.GetComponent<ViewModelComponent>();
                 if (viewModelComponent != null && viewModelComponent.ViewModel == itemViewModel)
                 {
-                    scrollRect.FocusOnChild(child as RectTransform, focusMargin);
+                    scrollRect.FocusOnChild(child as RectTransform, focusMargin, focusPointOffset);
                     return true;
                 }
             }
-            
+
             return false;
         }
 
         private void ResetScrollIfNeeded()
         {
-            if (!resetScrollWhenNothingFocused)
+            switch (nothingSelectedBehaviorMode)
             {
-                return;
+                case NothingSelectedBehaviorMode.ResetScroll:
+                    scrollRect.content.anchoredPosition = Vector2.zero;
+                    break;
+                case NothingSelectedBehaviorMode.KeepCurrent:
+                default:
+                    // Do nothing
+                    break;
             }
-            
-            scrollRect.content.anchoredPosition = Vector2.zero;
         }
 
 #if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            ScrollRect targetScrollRect = scrollRect != null ? scrollRect : GetComponent<ScrollRect>();
+            if (targetScrollRect == null || targetScrollRect.viewport == null)
+            {
+                return;
+            }
+
+            Rect focusZone = targetScrollRect.GetFocusZone(focusMargin, focusPointOffset);
+            Vector3 bottomLeft = new Vector3(focusZone.xMin, focusZone.yMin, 0f);
+            Vector3 topLeft = new Vector3(focusZone.xMin, focusZone.yMax, 0f);
+            Vector3 topRight = new Vector3(focusZone.xMax, focusZone.yMax, 0f);
+            Vector3 bottomRight = new Vector3(focusZone.xMax, focusZone.yMin, 0f);
+
+            Color color = Gizmos.color;
+            Matrix4x4 matrix = Gizmos.matrix;
+
+            Gizmos.color = Color.white;
+            Gizmos.matrix = targetScrollRect.viewport.localToWorldMatrix;
+            Gizmos.DrawLine(bottomLeft, topLeft);
+            Gizmos.DrawLine(topLeft, topRight);
+            Gizmos.DrawLine(topRight, bottomRight);
+            Gizmos.DrawLine(bottomRight, bottomLeft);
+            DrawFocusPointGizmo(focusZone.center, targetScrollRect.viewport.rect);
+
+            Gizmos.matrix = matrix;
+            Gizmos.color = color;
+        }
+
+        private static void DrawFocusPointGizmo(Vector2 center, Rect viewportRect)
+        {
+            float size = Mathf.Max(5f, Mathf.Min(viewportRect.width, viewportRect.height) * 0.03f);
+            Vector3 left = new Vector3(center.x - size, center.y, 0f);
+            Vector3 right = new Vector3(center.x + size, center.y, 0f);
+            Vector3 bottom = new Vector3(center.x, center.y - size, 0f);
+            Vector3 top = new Vector3(center.x, center.y + size, 0f);
+
+            Gizmos.DrawLine(left, right);
+            Gizmos.DrawLine(bottom, top);
+        }
+
         [MenuItem("CONTEXT/ScrollRect/Add Binder")]
         private static void AddBinder(MenuCommand command)
         {
--- Packages/Mace/Runtime/Utils/ExtensionMethods/ScrollRectExtensions.cs
+++ Packages/Mace/Runtime/Utils/ExtensionMethods/ScrollRectExtensions.cs
@@ -7,6 +7,11 @@ namespace Mace.Utils
     {
         public static void FocusOnChild(this ScrollRect scrollRect, RectTransform child, Vector2 margin)
         {
+            scrollRect.FocusOnChild(child, margin, Vector2.zero);
+        }
+
+        public static void FocusOnChild(this ScrollRect scrollRect, RectTransform child, Vector2 margin, Vector2 focusPointOffset)
+        {
             if (child == null)
             {
                 return;
@@ -15,47 +20,45 @@ namespace Mace.Utils
             Canvas.ForceUpdateCanvases();
             scrollRect.StopMovement();
 
-            // Get min and max of the viewport and child in local space to the viewport so we can compare them.
-            // NOTE: use viewport instead of the scrollRect as viewport doesn't include the scrollbars in it.
-            Vector2 viewPosMin = scrollRect.viewport.rect.min;
-            Vector2 viewPosMax = scrollRect.viewport.rect.max;
-
-            Vector2 childPosMin = scrollRect.viewport.InverseTransformPoint(child.TransformPoint(child.rect.min));
-            Vector2 childPosMax = scrollRect.viewport.InverseTransformPoint(child.TransformPoint(child.rect.max));
-
-            childPosMin -= margin;
-            childPosMax += margin;
+            RectTransform viewport = scrollRect.viewport;
+            Rect focusZone = scrollRect.GetFocusZone(margin, focusPointOffset);
+            Vector2 focusPoint = focusZone.center;
+            Vector2 childCenter = viewport.InverseTransformPoint(child.TransformPoint(child.rect.center));
 
             Vector2 move = Vector2.zero;
 
-            // Check if one (or more) of the child bounding edges goes outside the viewport and
-            // calculate move vector for the content rect so it can keep it visible.
-            if (childPosMax.y > viewPosMax.y)
+            if (scrollRect.vertical && !IsInside(childCenter.y, focusZone.yMin, focusZone.yMax))
             {
-                move.y = childPosMax.y - viewPosMax.y;
+                move.y = childCenter.y - focusPoint.y;
             }
 
-            if (childPosMin.x < viewPosMin.x)
+            if (scrollRect.horizontal && !IsInside(childCenter.x, focusZone.xMin, focusZone.xMax))
             {
-                move.x = childPosMin.x - viewPosMin.x;
-            }
-
-            if (childPosMax.x > viewPosMax.x)
-            {
-                move.x = childPosMax.x - viewPosMax.x;
-            }
-
-            if (childPosMin.y < viewPosMin.y)
-            {
-                move.y = childPosMin.y - viewPosMin.y;
+                move.x = childCenter.x - focusPoint.x;
             }
 
             // Transform the move vector to world space, then to content local space (in case of scaling or rotation?) and apply it.
-            Vector3 worldMove = scrollRect.viewport.TransformDirection(move);
+            Vector3 worldMove = viewport.TransformDirection(move);
             scrollRect.content.localPosition -= scrollRect.content.InverseTransformDirection(worldMove);
 
             Vector2 rawNormalizedPosition = scrollRect.normalizedPosition;
             scrollRect.normalizedPosition = new Vector2(Mathf.Clamp01(rawNormalizedPosition.x), Mathf.Clamp01(rawNormalizedPosition.y));
+        }
+
+        public static Rect GetFocusZone(this ScrollRect scrollRect, Vector2 margin, Vector2 focusPointOffset)
+        {
+            Vector2 focusMargin = new Vector2(Mathf.Abs(margin.x), Mathf.Abs(margin.y));
+            Vector2 focusPoint = scrollRect.viewport.rect.center + focusPointOffset;
+            return Rect.MinMaxRect(
+                focusPoint.x - focusMargin.x,
+                focusPoint.y - focusMargin.y,
+                focusPoint.x + focusMargin.x,
+                focusPoint.y + focusMargin.y);
+        }
+
+        private static bool IsInside(float value, float min, float max)
+        {
+            return value >= min && value <= max;
         }
     }
 }
